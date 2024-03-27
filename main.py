26830d0fc43dbceeb22141f5560b73af33f7b242
Circuitbreaker08 <circuitbreaker08@gmail.com>
2024-03-26 22:23:15 -0400

    Permitted cross origin

--- main.py
+++ main.py
@@ -2,8 +2,13 @@ import socketio
 import eventlet
 import threading
 
-sio = socketio.Server(logger=False, async_mode='eventlet')
+sio = socketio.Server(logger=False, async_mode='eventlet', cors_allowed_origins="*")
 app = socketio.WSGIApp(sio)
+
+@sio.event
+def connect(sid, environ, auth):
+    #print(sid)
+    pass
 
 def server():
     eventlet.wsgi.server(eventlet.listen(("", 2700)), app)
